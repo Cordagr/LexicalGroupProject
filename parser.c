d392f8a10adc8213ee3d5ac2fa36b7c5a602c5c2
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-03-18 14:12:34 -0500

    Update parser.c
    implementing parser

--- parser.c
+++ parser.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "calcy.tab.h"
-
+#include <ctype.h>
+#include "lex.yy.c"
 
 nodeType *createNewNumNode(int value);
 nodeType *createNewOprNode(int oper, nodeType *leftOp, nodeType *rightOp);
@@ -9,148 +10,153 @@ nodeType *expression();
 nodeType *term();
 nodeType *factor();
 nodeType *integer();
-//extern int yylval; 
-//extern yylex(void); //(from lexical analyzer) yylval stores value of token and yylex() gets next token
-extern FILE *yyin; //The file input
+extern FILE *yyin; 
 int token;
 
-
-//Creates a new number node, initializes the constant value
 nodeType *createNewNumNode(int value)
 {
-	nodeType *newNode = (nodeType *)malloc(sizeof(nodeType));
-	newNode->type=typeCon;
-	newNode->con.value=value;
-	return newNode;
+    nodeType *newNode = (nodeType *)malloc(sizeof(nodeType));
+    newNode->type = typeCon;
+    newNode->value = value;  
+    return newNode;
 }
 
-//Creates a new operator node, initializes the left and right operands
-nodeType *createNewOprNode(int oper, nodeType *leftOp, nodeType *rightOp)
-{
-	nodeType *newNode = (nodeType *)malloc(sizeof(nodeType));
-	newNode->type=typeOpr;
-	newNode->opr.oper=oper;
-	newNode->opr.nops=2;
-	newNode->opr.op[0]=leftOp;
-	newNode->opr.op[1]=rightOp;
-	return newNode;
-}
-
-//This function corresponds to the grammar rules <expression> and <expression_suffix>
-nodeType *expression()
-{
-	nodeType *newNode=term(); //store parsed term
-	
-	//As long as the token is + or -, get next token and make a node
-	while(token=='+' || token=='-')
-	{
-		int oper=token;
-		token=yylex();
-		nodeType *rightTerm = term();
-		if (!rightTerm)
-		{
-			printf("Syntax error expected right term \n");
-			exit(1);
-		}
-		newNode=createNewOprNode(oper, newNode, rightTerm);
-	}
-	
-	return newNode;
-}
-
-//This function corresponds to the grammar rules <term> and <term_suffix>
-nodeType *term()
-{
-	nodeType *newNode=factor();
-
-	//As long as the token is * or /, get next token and make a node
-	while(token=='*' || token=='/')
-	{
-		int oper=token;
-		token=yylex();
-		newNode=createNewOprNode(oper, newNode,factor());
-	}
-	return newNode;
-}
-
-nodeType *factor()
-{
-    nodeType * newNode;
-    if(token == '(')
-    {
-        token = yylex();
-        newNode = expression(); // comsume expression between parenthesis
-        if (token == ')')
-        {
-            token = yylex(); // consume )
-        }
-        else
-        {
-            printf("Error: Missing parenthesis"); // no closing parenthesis matching
-            exit(1);
-        }
-    }
-    else if (token == INTEGER)
-    {
-        newNode = integer(); // create integer with lexeme value
-    }
-    else
-    {
-        printf("Error: Invalid tokens in factor"); // no valid tokens for factor
+nodeType *createNewOprNode(int oper, nodeType *leftOp, nodeType *rightOp) {
+    nodeType *newNode = (nodeType *)malloc(sizeof(nodeType));
+    if (!newNode) {
+        printf("Memory allocation failed for operator node.\n");
         exit(1);
     }
-	return newNode;
+
+    if (leftOp != NULL) {
+        printf("Left operand value: %d\n", leftOp->value);
+    } else {
+        printf("Left operand is NULL\n");
+    }
+
+    if (rightOp != NULL) {
+        printf("Right operand value: %d\n", rightOp->value);
+    } else {
+        printf("Right operand is NULL\n");
+    }
+
+    newNode->type = typeOpr;
+    newNode->opr.oper = oper; 
+    newNode->opr.nops = 2;    
+
+    newNode->opr.op[0] = leftOp;
+    newNode->opr.op[1] = rightOp;
+
+    return newNode;
 }
 
-nodeType *integer()
+nodeType *expression() {
+    nodeType *newNode = term();
+
+    while (token == PLUS || token == MINUS) {
+        printf("Found operator: %d\n", token); 
+        int oper = token;  
+        token = yylex();   
+        nodeType *secondNode = term();
+        if (secondNode == NULL) {
+            printf("Failed to parse second term.\n");
+            exit(1);
+        }
+        newNode = createNewOprNode(oper, newNode, secondNode);  
+        printf("New node value: %d\n", newNode->opr.op[0]->value); 
+    }
+    return newNode;
+}
+
+nodeType *term() {
+    printf("Token at term: %d\n", token);
+    nodeType *newNode = factor();
+
+    while (token == MULT || token == DIV) {
+        int oper = token;
+        token = yylex();
+        newNode = createNewOprNode(oper, newNode, factor());
+    }
+    return newNode;
+}
+
+nodeType *factor() {
+    printf("Entering factor()\n");
+    printf("Current token in factor: %d\n", token);
+
+    nodeType *newNode;
+    if (token == '(') {  
+        token = yylex(); 
+        newNode = expression(); 
+        if (token == ')') {
+            token = yylex(); 
+        } else {
+            printf("Syntax error: Expected ')'\n");
+            exit(1); 
+        }
+    }
+    else if (token == INTEGER) { 
+        printf("Found INTEGER token: %d\n", yylval.iValue);
+        newNode = integer(); 
+    }
+    else {
+        printf("Error: Invalid token in factor: %d\n", token);
+        exit(1); 
+    }
+
+    return newNode;
+}
+
+nodeType *integer() {
+    int value = yylval.iValue;  
+    nodeType *newNode = createNewNumNode(value);  
+    printf("Created new node with value: %d\n", newNode->value);
+    printf("Current token: %d\n", token);
+    token = yylex();  
+    printf("Next token: %d\n", token);
+    return newNode;
+}
+
+void printAST(nodeType *node, int depth)
 {
-  if(token == INTEGER)
-  {
-	int value = yylval.iValue; // to integer
-	token = yylex();
-	return createNewNumNode(value);
-  }
-  else
-  {
-	printf("Invalid tokens in integer call");
-	exit(1);
-  }
+    if (!node)
+        return;
+
+    for (int i = 0; i < depth; i++)
+        printf("    ");
+
+    if (node->type == typeCon)
+    {
+        printf("%d (int)\n", node->value); 
+    }
+    else if (node->type == typeOpr)
+    {
+        printf("%s (operator)\n", 
+               (node->opr.oper == PLUS) ? "+" : (node->opr.oper == MINUS) ? "-"
+                                         : (node->opr.oper == MULT)   ? "*"
+                                         : (node->opr.oper == DIV)   ? "/"
+                                         : "?");
+
+        if (node->opr.op[0]) 
+            printAST(node->opr.op[0], depth + 1);
+        if (node->opr.op[1])
+            printAST(node->opr.op[1], depth + 1);
+    }
 }
 
-
-void printAST(nodeType * node, int depth)
+int main()
 {
-if (!node)
-return;
+    printf("Starting parser...\n");
+    printf("Enter expression: ");
 
-// indentation //
-for (int i=0;i<depth;i++)
-{
-	printf("    ");
-}
-	
-if (node -> type == typeCon)
-{
-	printf("%d(int)\n", node -> con.value);
-}
-else if (node -> type == typeOpr)
-{
-	printf("%c(operator)\n", node -> opr.oper);
+    yyin = stdin; 
 
-	// left opr recursive call //
-	printAST(node->opr.op[0], depth + 1);
-	// right opr recursive call //
-	printAST(node->opr.op[1], depth + 1);		
-}
-}
+    token = yylex();
 
-int main(int argc, char *argv[])
-{ 
-// TODO: call lexical reading from input and get tokens from lexical program and then call 
-token = yylex();
-nodeType *ast = expression();
-printAST(ast,0);
-return 0;
+    nodeType *ast = expression();
+
+    printf("\nAbstract Syntax Tree:\n");
+    printAST(ast, 0);
+
+    return 0;
 }
-
-
